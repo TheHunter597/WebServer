abf995cd8fe155ecad13951aa4c7ff25ba1de087
TheHunter597 <thehunter597777@gmail.com>
2026-01-27 19:45:10 +0200

    added some routes to App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -29,81 +29,81 @@ public class App {
     public static void main(String[] args) throws IOException {
         Server server = new Server(3);
         server.enableDatabaseConnection();
-        // server.addRoute("GET", "/", (req, res) -> {
-        // res.setStatusCode(200);
-        // try {
-        // res.httpFileResponse("/index.html");
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-        // return res;
+        server.addRoute("GET", "/", (req, res) -> {
+            res.setStatusCode(200);
+            try {
+                res.httpFileResponse("/index.html");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return res;
 
-        // });
-        // server.get("/json", (req, res) -> {
-        // HashMap<String, Object> map = new HashMap<>();
-        // map.put("name", "Mohamed");
-        // res.json(map);
-        // return res;
-        // });
+        });
+        server.get("/json", (req, res) -> {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("name", "Mohamed");
+            res.json(map);
+            return res;
+        });
 
-        // server.get("/json/?id=str", (req, res) -> {
-        // HashMap<String, Object> map = new HashMap<>();
-        // map.put("name", "Mohamed");
-        // res.json(map);
-        // return res;
-        // });
-        // server.get("/json/?id=str/?age=int", (req, res) -> {
-        // HashMap<String, Object> map = new HashMap<>();
-        // map.put("name", "Mohamed");
-        // res.json(map);
-        // return res;
-        // });
-        // server.post("/data", (req, res) -> {
-        // HashMap<String, Object> map = new HashMap<>();
-        // map.put("name", "Mohamed");
-        // res.json(map);
-        // return res;
-        // });
-        // server.post("/upload", (req, res) -> {
-        // res.setHeader("Authorization", "test");
-        // return res;
-        // });
-        // server.post("/upload/:data", (req, res) -> {
-        // res.setHeader("Authorization", "test");
-        // return res;
-        // });
-        // server.post("/upload/:data/:mango", (req, res) -> {
-        // res.setHeader("Authorization", "test");
-        // return res;
-        // });
+        server.get("/json/?id=str", (req, res) -> {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("name", "Mohamed");
+            res.json(map);
+            return res;
+        });
+        server.get("/json/?id=str/?age=int", (req, res) -> {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("name", "Mohamed");
+            res.json(map);
+            return res;
+        });
+        server.post("/data", (req, res) -> {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("name", "Mohamed");
+            res.json(map);
+            return res;
+        });
+        server.post("/upload", (req, res) -> {
+            res.setHeader("Authorization", "test");
+            return res;
+        });
+        server.post("/upload/:data", (req, res) -> {
+            res.setHeader("Authorization", "test");
+            return res;
+        });
+        server.post("/upload/:data/:mango", (req, res) -> {
+            res.setHeader("Authorization", "test");
+            return res;
+        });
 
-        // server.get("/users", (req, res, db) -> {
-        // try {
-        // var users = db.query(
-        // "SELECT id, username, password FROM CUSTOM_USERS",
-        // rs -> {
-        // var usersList = new java.util.ArrayList<User>();
-        // while (rs.next()) {
-        // User user = new User();
-        // user.setId(rs.getLong("id"));
-        // user.setUsername(rs.getString("username"));
-        // user.setPassword(rs.getString("password"));
-        // usersList.add(user);
-        // }
-        // return usersList;
-        // });
-        // res.setStatusCode(200);
-        // res.json(users);
-        // return res;
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // res.setStatusCode(500);
-        // HashMap<String, String> errorResponse = new HashMap<>();
-        // errorResponse.put("error", "Error retrieving users: " + e.getMessage());
-        // res.json(errorResponse);
-        // return res;
-        // }
-        // });
+        server.get("/users", (req, res, db) -> {
+            try {
+                var users = db.query(
+                        "SELECT id, username, password FROM CUSTOM_USERS",
+                        rs -> {
+                            var usersList = new java.util.ArrayList<User>();
+                            while (rs.next()) {
+                                User user = new User();
+                                user.setId(rs.getLong("id"));
+                                user.setUsername(rs.getString("username"));
+                                user.setPassword(rs.getString("password"));
+                                usersList.add(user);
+                            }
+                            return usersList;
+                        });
+                res.setStatusCode(200);
+                res.json(users);
+                return res;
+            } catch (Exception e) {
+                e.printStackTrace();
+                res.setStatusCode(500);
+                HashMap<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("error", "Error retrieving users: " + e.getMessage());
+                res.json(errorResponse);
+                return res;
+            }
+        });
 
         server.post("/users/update", (req, res, db) -> {
             var requestData = req.getBodyAsJson(RequestUser.class);
